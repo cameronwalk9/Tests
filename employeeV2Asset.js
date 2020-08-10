f2e7db10f0f290c986ce20bb2ce66d60f12cf00d
cameronwalk9 <68448132+cameronwalk9@users.noreply.github.com>
2020-08-10 11:21:32 -0600

    Add files via upload

---
+++ employeeV2Asset.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        name:'Cameron Walk',
+        phoneNumber:'2222222222',
+        email:'@gmail.com',
+        title:'yamomma',
+    },
+    {
+    name:'Bond James',
+    phoneNumber:'0070070070',
+    email:'@goldeneye.com',
+    title:'007',
+    }
+]
